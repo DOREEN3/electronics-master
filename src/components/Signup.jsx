74de56ffa636cbf69dce731176e82412977eced8
DOREEN3 <142734037+DOREEN3@users.noreply.github.com>
2025-12-02 20:12:08 +0300

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -128,7 +128,10 @@ const Signup = () => {
     <div className='container-fluid d-flex justify-content-center align-items-center py-5 px-3'>
 
       
-        <form onSubmit={handleSubmit} className='border rounded shadow p-4 w-100 w-md-50'>
+        <form onSubmit={handleSubmit} className='border rounded shadow p-4'
+             styles={{ width: '100%',         // full width on very small screens
+              maxWidth: '450px',     // never exceed 450px on desktop
+            minWidth: '300px'}}>
             <fieldset>
                 <legend className='text-center fs-3 fw-bold'>Sign Up</legend>
                   {/* bind the states  */}
@@ -192,3 +195,4 @@ const Signup = () => {
 
 
 export default Signup
+
