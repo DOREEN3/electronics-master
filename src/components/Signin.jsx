95bbaa2c877af781ed26b14a018fb3c2c1ee924d
DOREEN3 <142734037+DOREEN3@users.noreply.github.com>
2025-12-03 08:51:50 +0300

    Update Signin.jsx

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -46,9 +46,8 @@ const Signin = () => {
 
   return (
     <div className='container-fluid d-flex justify-content-center align-items-center py-5 px-3'>
-      <form onSubmit={handleSubmit} className='border rounded shadow p-4' 
-        styles={{ width: '100%',         // full width on very small screens
-              maxWidth: '50%',     // never exceed 450px on desktop
+      <form onSubmit={handleSubmit} className='border rounded shadow p-4 w-50' 
+        styles={{ 
             minWidth: '300px'}}>
         <fieldset>
           <legend className='text-center fs-3 fw-bold'>Sign In</legend>
@@ -87,6 +86,7 @@ const Signin = () => {
 
 
 export default Signin
+
 
 
 
